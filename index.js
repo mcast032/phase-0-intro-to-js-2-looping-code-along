bfc687e04edbc04d4a3556b89b245584a64bb0d4
Mario Castro <mcast032@gmail.com>
2024-05-13 14:02:36 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+function writeCards(names, birthday) {
+    
+    let messages = [];
+
+    
+    for (let name of names) {
+        
+        messages.push(`Thank you, ${name}, for the wonderful ${birthday} gift!`);
+    }
+
+  
+    return messages;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
